81b946696c6f9706d1cbf92ac539e3b267958bfb
Sunder Rawat <sunderrawat777@gmail.com>
2021-03-22 20:03:04 +0530

    add new features in model data validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,21 +5,40 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true,
 });
 
-// // create a user model
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
+// create a user model
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [7, 'Length is more than 6'],
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password not contains password');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be greather or equal zero');
+      }
+    },
+  },
+});
 
 // const me = new User({
-//   name: 'sunder',
-//   age: 25,
+//   name: 'Ravi',
+//   age: 24,
+//   password: 'goodPAssword',
 // });
 
+// //save user on database
 // me.save()
 //   .then(() => console.log(me))
 //   .catch(error => console.log(error));
@@ -29,28 +48,32 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const Task = mongoose.model('Tasks', {
   Description: {
     type: String,
+    required: true,
+    trim: true,
+    minLength: [3, 'length more than 3']
   },
   Completed: {
     type: Boolean,
+    default: false
   },
 });
 
-const task1 = new Task({
-  Description: 'Dud pilana',
-  Completed: false,
-});
+// const task1 = new Task({
+//   Description: 'Dud pilana',
+//   Completed: false,
+// });
 
-const task2 = new Task({
-  Description: 'Sarso katni',
+const task3 = new Task({
+  Description: '   Sarso katni  or kaat di    ',
   Completed: true,
 });
 
-task1
+// save task on database
+// task1
+//   .save()
+//   .then(() => console.log(task1))
+//   .catch(error => console.log(error));
+task3
   .save()
-  .then(() => console.log(task1))
+  .then(() => console.log(task3))
   .catch(error => console.log(error));
-task2
-  .save()
-  .then(() => console.log(task2))
-  .catch(error => console.log(error));
-
