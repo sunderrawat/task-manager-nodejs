860797ca02420c14a7344e420a29dfea45b1e671
Sunder Rawat <sunderrawat777@gmail.com>
2021-03-23 23:52:04 +0530

    make code clean and transfer routes in diffrent file

---
+++ src/routers/task.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const Task = require('../model/task');
+
+const router = new express.Router();
+
+// make a post request endpoint for tasks
+router.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//reading endpoint for fetching all tasks
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+//reading single task from id
+router.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+//update task
+router.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['completed', 'description'];
+  const isValidOperations = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperations) {
+    return res.status(400).send({ error: 'Indvaild operation' });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//delete single task from tasks
+router.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const User = require('../model/user');
+
+const router = new express.Router();
+
+// make a post request endpoint for users
+router.post('/users', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(201).send(e);
+  }
+});
+
+//make endpoint for reading all users data
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// GET one user from id
+router.get('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+//update user from id
+router.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'age', 'email', 'password'];
+  const isValidOperations = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperations) {
+    return res.status(400).send({ error: 'Indvaild operation' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//delete one user from database
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
