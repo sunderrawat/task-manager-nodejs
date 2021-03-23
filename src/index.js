9baa33cc06ea59518a7de2be08c4d07513de2acb
Sunder Rawat <sunderrawat777@gmail.com>
2021-03-23 23:24:28 +0530

    add new features update and delete task and users || router before seprating

--- src/index.js
+++ src/index.js
@@ -74,6 +74,44 @@ app.get('/users/:id', async (req, res) => {
   //   });
 });
 
+//update user from id
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'age', 'email', 'password'];
+  const isValidOperations = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperations) {
+    return res.status(400).send({ error: 'Indvaild operation' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//delete one user from database
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//---------Tasks------------
 // make a post request endpoint for tasks
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
@@ -133,4 +171,41 @@ app.get('/tasks/:id', async (req, res) => {
   //   .catch(e => {
   //     res.send(e);
   //   });
+});
+
+//update task
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['completed', 'description'];
+  const isValidOperations = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperations) {
+    return res.status(400).send({ error: 'Indvaild operation' });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//delete single task from tasks
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
